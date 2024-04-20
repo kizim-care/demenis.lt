28ed377290a8c624150b1e5ec77ee78c50f67431
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-04-20 14:03:55 +0100

    added build settings

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+//  @type {import('tailwindcss').Config}
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
