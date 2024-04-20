1360ebf00c90bbfd8e3f5ee454a8e7e2ea7b7386
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-04-20 17:55:00 +0100

    changed scripts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 //  @type {import('tailwindcss').Config}
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.js"],
+  content: ["./src/**/*.html", "./src/**/*.js"],
   theme: {
     extend: {},
   },
